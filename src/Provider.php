8c7a016b977951f98274e432d9579c3a682d92f6
Charles Lalonde <charles@pickcrew.com>
2015-07-29 22:56:02 -0400

    Use https in production.

--- src/Provider.php
+++ src/Provider.php
@@ -27,7 +27,7 @@ class Provider extends AbstractProvider
 	 */
 	public function urlAuthorize()
 	{
-		return 'http://unsplash.com/oauth/authorize';
+		return 'https://unsplash.com/oauth/authorize';
 	}
 
 	/**
@@ -37,7 +37,7 @@ class Provider extends AbstractProvider
 	 */
 	public function urlAccessToken()
 	{
-		return 'http://unsplash.com/oauth/token';
+		return 'https://unsplash.com/oauth/token';
 	}
 
 	/**
@@ -47,7 +47,7 @@ class Provider extends AbstractProvider
 	 */
 	public function urlUserDetails(AccessToken $token)
 	{
-		return "http://api.unsplash.com/me?access_token={$token}";
+		return "https://api.unsplash.com/me?access_token={$token}";
 	}
 
 	/**
