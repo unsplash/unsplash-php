468cf55833f8a25d6c8967949a53a597d4bfe042
AMS777 <alberto@oscuro.es>
2018-01-29 14:24:38 +0100

    Add 'orientation' and 'collections' parameters to photo search. Update README.md.

--- src/Search.php
+++ src/Search.php
@@ -12,21 +12,33 @@ class Search extends Endpoint
      * Retrieve a single page of photo results depending on search results
      * Returns ArrayObject that contain PageResult object.
      *
-     * @param  string  $search   Search terms.
-     * @param  integer $page     Page from which the photos need to be retrieve
-     * @param  integer $per_page Number of element in a page
+     * @param  string  $search       Search terms.
+     * @param  integer $page         Page number to retrieve. (Optional; default: 1)
+     * @param  integer $per_page     Number of items per page. (Optional; default: 10)
+     * @param  string  $orientation  Filter search results by photo orientation. Valid values are landscape,
+     *                               portrait, and squarish. (Optional)
+     * @param  string  $collections  Collection ID(‘s) to narrow search. If multiple, comma-separated. (Optional)
      * @return PageResult
      */
-    public static function photos($search, $page = 1, $per_page = 10)
+    public static function photos($search, $page = 1, $per_page = 10, $orientation = null, $collections = null)
     {
+        $query = [
+            'query' => $search,
+            'page' => $page,
+            'per_page' => $per_page
+        ];
+
+        if ( ! empty($orientation)) {
+            $query['orientation'] = $orientation;
+        }
+
+        if ( ! empty($collections)) {
+            $query['collections'] = $collections;
+        }
+
         $photos = self::get(
             "/search/photos",
-            [ 'query' => [
-                    'query' => $search,
-                    'page' => $page,
-                    'per_page' => $per_page
-                ]
-            ]
+            [ 'query' => $query ]
         );
 
         return self::getPageResult($photos->getBody(), $photos->getHeaders(), Photo::class);
